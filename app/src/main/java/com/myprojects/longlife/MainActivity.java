320ad969aa094a85183af0377a44cc35892cd3b0
semyonmoroshek <s.moroseks@gmail.com>
2020-02-28 21:35:28 +0200

    Git test

---
+++ app/src/main/java/com/myprojects/longlife/MainActivity.java
@@ -0,0 +1,19 @@
+package com.myprojects.longlife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i("Test", "test");
+
+
+    }
+}
