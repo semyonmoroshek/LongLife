edc4aa345e2dc2763d6b15029711f8f799e1ca0d
semyonmoroshek <s.moroseks@gmail.com>
2020-02-28 21:54:58 +0200

    Git test

--- app/src/main/java/com/myprojects/longlife/MainActivity.java
+++ app/src/main/java/com/myprojects/longlife/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txt = findViewById(R.id.txt_title);
     }
 }
